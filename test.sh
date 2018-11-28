2ab4a1bee24107233a13c20cf155888d073ab841
xuchengyun <chengyun.xu@bitmain.com>
2018-11-28 19:28:22 +0800

    init

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-lastTime= date +%Y-%m-%d-%H-%M
+lastTime=`date "+%Y%m%d%H%M"`
+echo $lastTime
 
-git tag -a '$lastTime' -m 'prod $lastTime'
+git tag -a "$lastTime" -m "prod $lastTime"
+
